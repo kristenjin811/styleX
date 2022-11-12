99b81618190d954fcc0fff230d2da54720535bec
Wenqi Jin <kristenjinn@gmail.com>
2022-11-11 18:02:55 -0800

    Authentication completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import register, login, logout
+from .views import register, login, logout, activate, dashboard, forgotPassword, resetPassword_validate, resetPassword
 
 urlpatterns = [
   path('register/', register, name='register'),
   path('login/', login, name='login'),
   path('logout/', logout, name='logout'),
+  path('dashboard/', dashboard, name='dashboard'),
+  path('', dashboard, name='dashboard'),
 
-
+  path('forgotPassword/', forgotPassword, name='forgotPassword'),
+  path('activate/<uidb64>/<token>/', activate, name='activate'),
+  path('resetPassword_validate/<uidb64>/<token>/', resetPassword_validate, name='resetPassword_validate'),
+  path('resetPassword/', resetPassword, name='resetPassword'),
 ]
--- stylex/settings.py
+++ stylex/settings.py
@@ -136,3 +136,15 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# SMTP Configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'host_email'
+EMAIL_HOST_PASSWORD = 'host_email_password'
+EMAIL_USE_TLS = True
