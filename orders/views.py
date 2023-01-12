162e63aaeb6001984712ae0ff2cb826fddd9e277
Wenqi Jin <kristenjinn@gmail.com>
2023-01-12 13:20:19 -0800

    order confirmation and redirect user to invoice page completed

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from carts.models import CartItem
 from .forms import OrderForm
 from .models import Order, Payment, OrderProduct
 from store.models import Product
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 import datetime
 import json
 
@@ -55,8 +57,22 @@ def payments(request):
   # delete the cart items of this particular user
   CartItem.objects.filter(user=request.user).delete()
 
+  # send user order confirmation email
+  mail_subject = "Thank you for shopping with us!"
+  message = render_to_string('orders/order_received_email.html', {
+    'user': request.user,
+    'order': order,
+  })
+  to_email = request.user.email
+  send_email = EmailMessage(mail_subject, message, to=[to_email])
+  send_email.send()
 
-  return render(request, 'orders/payments.html')
+  # send order number and transaction id back to sendData method via JsonResponse
+  data = {
+    'order_number': order.order_number,
+    'transID': payment.payment_id,
+  }
+  return JsonResponse(data)
 
 
 # default value of totla and quantity are both 0
@@ -118,3 +134,32 @@ def place_order(request, total=0, quantity=0,):
       return render(request, 'orders/payments.html', context)
   else:
     return redirect('checkout')
+
+
+def order_complete(request):
+  # store the values from url to new variables
+  order_number = request.GET.get('order_number')
+  transID = request.GET.get('payment_id')
+
+  try:
+    order = Order.objects.get(order_number=order_number, is_ordered=True)
+    ordered_products = OrderProduct.objects.filter(order_id=order.id)
+
+    subtotal = 0
+    for i in ordered_products:
+      subtotal += i.product_price * i.quantity
+
+    payment = Payment.objects.get(payment_id=transID)
+
+    context = {
+      'order': order,
+      'ordered_products': ordered_products,
+      'order_number': order.order_number,
+      'transID': payment.payment_id,
+      'payment': payment,
+      'subtotal': subtotal, 
+    }
+
+    return render(request, 'orders/order_complete.html', context)
+  except (Payment.DoesNotExist, Order.DoesNotExist):
+    return redirect('home')
--- stylex/settings.py
+++ stylex/settings.py
@@ -146,6 +146,6 @@ MESSAGE_TAGS = {
 # SMTP Configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'email host user'
+EMAIL_HOST_USER =  'email host user'
 EMAIL_HOST_PASSWORD = 'email host password'
 EMAIL_USE_TLS = True
